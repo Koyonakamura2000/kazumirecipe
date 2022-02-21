048670f202a5d1c70c09ee6f1cf0e29c11c36294
Koyo Nakamura <koyonakamura123@gmail.com>
2022-02-21 14:55:25 -0800

    mvp version complete

--- src/Title.js
+++ src/Title.js
@@ -1,5 +1,13 @@
+import './Title.css';
+
 function Title() {
-    return <div>title</div>;
+    return (
+        <div className='title-container'>
+            <h1 className='title'>和美のレシピ</h1>
+            <hr/>
+            <h1 className='title-english'>Kazumi's Recipes</h1>
+        </div>
+    );
 }
 
 export default Title;
